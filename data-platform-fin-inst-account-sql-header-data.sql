756869a364ad65aa0d15cf37dc1617b34243d81d
MiyuYokoyama0109 <miyu.y@latona.io>
2022-10-11 19:00:23 +0900

    release

---
+++ data-platform-fin-inst-account-sql-header-data.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `data_platform_fin_inst_account_header_data`  -- 新規追加 
+(
+    `FinInstCountry`            varchar(3) NOT NULL,          -- 新規追加
+    `FinInstNumber`             varchar(15) NOT NULL,         -- 新規追加
+    `FinInstBusinessPartner`    int(10) NOT NULL,             -- 新規追加
+    `InternalFinInstCustomerID` int(10) NOT NULL,             -- 新規追加
+    `AccountBusinessPartner`    int(10) NOT NULL,             -- 新規追加
+    `ValidityEndDate`           varchar(80) NOT NULL,         -- 新規追加
+    `ValidityStartDate`         varchar(80) DEFAULT NULL,     -- 新規追加
+    `IsMarkedForDeletion`       tinyint(1) DEFAULT NULL,      -- 新規追加
+    PRIMARY KEY (`FinInstCountry`, `FinInstNumber`, `FinInstBusinessPartner`, `InternalFinInstCustomerID`, `AccountBusinessPartner`, `ValidityEndDate`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
